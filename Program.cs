d501918dbdcd81fab9b4d0eadc7ac5c16a8bd1b6
DenverJensen <jensendenver@gmail.com>
2021-09-05 21:39:23 -0600

    validate user input and finalize math

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Assignment1._2
 {
@@ -12,23 +13,83 @@ namespace Assignment1._2
         {
             //Declare variables
             //
-            int number1; //first number from user
-            int number2; // second number from user
-            int sum; //sum of user inputs
-            int diff;//difference of user inputs
+            string snumber1; //string of first number from user
+            string snumber2; // string of second number from user
+            double number1; //first number from user
+            double number2; // second number from user
+            double sum; //sum of user inputs
+            double diff;//difference of user inputs
+            double prod; //product of user inputs
+            double quot; // quotient of user inputs
+            double mod; //remainder of user inputs
 
-            Console.Write("Enter first number: "); //prompt
-            number1 = Convert.ToInt32(Console.ReadLine()); //read
 
-            Console.Write("Enter the second number: "); //2nd prompt
-            number2 = Convert.ToInt32(Console.ReadLine()); //2nd read
+
+            Console.WriteLine("Enter first number: "); //prompt
+            snumber1 = Console.ReadLine(); //read
+
+            //validate input
+            while (!Double.TryParse(snumber1, out number1))
+            {
+                Console.WriteLine("Not a valid number, try again.");
+                snumber1 = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Enter second number: "); //prompt
+            snumber2 = Console.ReadLine(); //read
+
+            //validate 2nd input
+            while (!Double.TryParse(snumber2, out number2))
+            {
+                Console.WriteLine("Not a valid number, try again.");
+                snumber1 = Console.ReadLine();
+            }
 
             sum = number1 + number2;
+            Console.WriteLine("{0} + {1} = {2}", number1, number2, sum);
+
             diff = number1 - number2;
+            Console.WriteLine("{0} - {1} = {2}", number1, number2, diff);
 
-            Console.WriteLine("Sum is {0}", sum);
-            Console.WriteLine("{0} + {0} = ", sum);
+            prod = number1 * number2;
+            Console.WriteLine("{0} * {1} = {2}", number1, number2, prod);
 
+            quot = number1 / number2;
+            Console.WriteLine("{0} / {1} = {2}", number1, number2, quot);
+
+            mod = number1 % number2;
+            Console.WriteLine("{0} % {1} = {2}", number1, number2, mod);
+
+            //check if greater than
+            if (number1 > number2)
+            {
+                Console.WriteLine("{0} is greater than {1}", number1, number2);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not greater than {1}", number1, number2);
+            }
+
+            // check if less than
+            if (number1 < number2)
+            {
+                Console.WriteLine("{0} is less than {1}", number1, number2);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not less than {1}", number1, number2);
+            }
+
+            //check if equal
+            if (number1 == number2)
+            {
+                Console.WriteLine("{0} is equal to {1}", number1, number2);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not equal to {1}", number1, number2);
+            }
         }
     }
 }
